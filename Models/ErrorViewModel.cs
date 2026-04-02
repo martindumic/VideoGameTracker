835bba086cbb2434002485decc2b351408837f2c
martindumic <mdumic@tvz.hr>
2026-04-02 15:42:31 +0200

    fix

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+using System.Diagnostics;
+
+namespace VideoGameTracker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
